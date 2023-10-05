38dbb9446e3153d7be92cc6281ae10c4df3c2dd9
glwhitaker <glwhitaker3@gmail.com>
2023-10-05 12:27:39 -0400

    remove redundancies

--- classes/matchup.py
+++ classes/matchup.py
@@ -7,9 +7,3 @@ class Matchup:
         self.date = game.find('tr',{'class':'date'}).td.string
         self.away = team.Team(game.tr.findNext('tr'))
         self.home = team.Team(game.tr.findNext('tr').findNext('tr'))
-        
-    
-    # string representation of a matchup
-    def __str__(self):
-        return self.date + "\n" + self.away.name + " " + self.away.score + \
-            "\n" + self.home.name + " " + self.home.score + "\n"
